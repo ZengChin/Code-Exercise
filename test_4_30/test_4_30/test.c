f5464c55b73d83669bf31273216e730ea3d39808
ZengChin <154317601+ZengChin@users.noreply.github.com>
2024-04-30 23:43:36 +0800

    4月30日的练习
    开始学链表了！

---
+++ test_4_30/test_4_30/test.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int x = 0;
+	char a = 'e';
+	float f = 2.12f;
+	double d = 2.1111;
+	long l = 23458;
+	int* px = &x;
+	char* pa = &a;
+	float* pf = &f;
+	double* pd = &d;
+	long* pl = &l;
+	printf(" %p\n %p\n %p\n %p\n %p\n", px, pa, pf, pd,pl);
+	//printf("%d\n%d\n%d\n%d\n%d\n", sizeof(px), sizeof(pa), sizeof(pf), sizeof(pd), sizeof(pl));
+	printf("%d", sizeof(x));
+	return 0;
+}
